ab163eed9aa63fdb3ccc823574c6983d17c9c63b
mhaligowski <mhaligowski@gmail.com>
2013-09-19 19:39:22 +0200

    initialize angular project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('fishkeesUiApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
