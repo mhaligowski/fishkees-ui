6bde2410cfd2855875f7860e8a5979e4437ade92
mhaligowski <mhaligowski@gmail.com>
2013-09-20 21:39:56 +0200

    Prepare add new flashcard list button

---
+++ test/e2e/add_new_flashcardlist.js
@@ -0,0 +1,14 @@
+describe('Add new flashcardlist scenarios', function() {
+ 
+  describe('The view', function() {
+ 
+    beforeEach(function() {
+      browser().navigateTo('/#/FlashcardLists');
+    });
+ 
+ 
+    it('should contain a button for adding new flashcardlist', function() {
+      expect(element('button.add-new-list').count()).toBe(1);
+    });
+  });
+});
