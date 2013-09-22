7e00e7c470c439519595cb7ca70fc0cca6778a6e
mhaligowski <mhaligowski@gmail.com>
2013-09-22 21:20:14 +0200

    Tests for add new list modal action

---
+++ test/spec/controllers/FlashcardListModalInstanceCtrl.js
@@ -0,0 +1,59 @@
+'use strict'
+
+describe('Controller: ModalInstanceCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('fishkeesUiApp.flascardList'));
+
+  var ModalInstanceCtrl,
+    scope,
+    mockModalInstance;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    // mock $modal
+    mockModalInstance =  {
+      dismiss: function(action) {},
+      close: function(someList) {}
+    };
+    
+    spyOn(mockModalInstance, 'dismiss');
+    spyOn(mockModalInstance, 'close');
+
+    // mock scope
+    scope = $rootScope.$new();
+
+    // mock controller
+    ModalInstanceCtrl = $controller('ModalInstanceCtrl', {
+      $scope: scope,
+      $modalInstance: mockModalInstance,
+    });
+  }));
+
+  it('should contain empty new list', function() {
+    expect(scope.newList).toEqual({
+      title: ''
+    });
+
+    expect(scope.newList).not.toEqual({
+      title: 'someTitle'
+    });
+  });
+
+  it('should call modal closing when cancel is called', function() {
+    // when
+    scope.cancel();
+
+    // then
+    expect(mockModalInstance.dismiss).toHaveBeenCalledWith('cancel');
+  });
+
+  it('should call modal closing with returning argument', function() {
+    scope.newList = {
+        title: 'newTestList'
+    };
+
+    scope.ok();
+    expect(mockModalInstance.close).toHaveBeenCalledWith(scope.newList);
+  });
+});
