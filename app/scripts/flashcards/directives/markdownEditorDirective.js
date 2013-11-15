57bf6757874cae4064e56621a73c8b48a515e4e2
mhaligowski <mhaligowski@gmail.com>
2013-11-16 00:05:49 +0100

    Make a single template for markdown editor

--- app/scripts/flashcards/directives/markdownEditorDirective.js
+++ app/scripts/flashcards/directives/markdownEditorDirective.js
@@ -4,18 +4,14 @@ angular
     .module('flashcardModule.directives')
     .directive('markdownEditor', function() {
         var converter = new Showdown.converter();
-        var previewTemplate = '<div ng-hide="isEditMode" ng-dblclick="toEditMode()" class="preview"></div>';
-        var editorTemplate = '<textarea ng-show="isEditMode" class="editor input-block-level"></textarea>';
 
         return {
             restrict: 'E',
             scope: {
                 text: '=text'
             },
+            template: '<div ng-hide="isEditMode" ng-dblclick="toEditMode()" class="preview"></div><textarea ng-show="isEditMode" class="editor input-block-level"></textarea>',
             compile: function(tElement, tAttrs, transclude) {
-                tElement.html(editorTemplate);
-                tElement.append(previewTemplate);
-
                 return function(scope, iElement, iAttrs, controller) {
                     scope.isEditMode = false;
 
