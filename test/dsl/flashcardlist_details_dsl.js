ffbf5999e02840f28fc442df411b9ff766a412b0
mhaligowski <mhaligowski@gmail.com>
2013-11-20 00:19:36 +0100

    Add test for showing button for empty list

--- test/dsl/flashcardlist_details_dsl.js
+++ test/dsl/flashcardlist_details_dsl.js
@@ -5,6 +5,14 @@ angular.scenario.dsl('fishkees', function() {
 
     api.addNewFlashcardButton = function() {
         return this.dsl.element(".add-flashcard.btn");
+    };
+
+    api.goToPageWithNoFlashcards = function() {
+        this.dsl.browser().navigateTo('/#/FlashcardList/someFlashcardListId2');
+    };
+
+    api.goToPageWithSomeFlashcards = function() {
+        this.dsl.browser().navigateTo('/#/FlashcardList/someFlashcardListId1');
     }
 
     return function() { return api; };
--- test/e2e/flashcard/add_flashcardlist_details.js
+++ test/e2e/flashcard/add_flashcardlist_details.js
@@ -1,14 +1,27 @@
 'use strict'
 
 describe('Add new flashcard', function() {
+    var _;
+
     beforeEach(function() {
+        _ = fishkees();
         browser().navigateTo('/');
         restartDB();
         sleep(0.3);
-        browser().navigateTo('/#/FlashcardList/someFlashcardListId1');
+        _.goToPageWithSomeFlashcards();
     });  
 
-    it('should show the add button', function() {
-        expect(fishkees().addNewFlashcardButton().count()).toBe(1);
+    it('should show the add button when there are some flashcards', function() {
+        // then
+        expect(_.addNewFlashcardButton().count()).toBe(1);
     });
+
+    it('should show the add button when there are no flashcards', function() {
+        // when
+        _.goToPageWithNoFlashcards();
+
+        // then
+        expect(_.addNewFlashcardButton().count()).toBe(1);
+    });
+
 });
