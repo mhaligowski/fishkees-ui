b08aa5fe49e25370ebeb7d7269088a637e1e2d72
mhaligowski <mhaligowski@gmail.com>
2014-02-13 23:41:03 +0100

    empty flashcard set e2e test

---
+++ test/e2e/player/empty_flashcard_set.js
@@ -0,0 +1,19 @@
+'use strict'
+
+xdescribe('Player with empty flashcard set', function() {
+    beforeEach(function() {
+        browser().navigateTo('/');
+        restartDB();
+        sleep(0.3);
+        browser().navigateTo('/#/Player/someFlashcardListId3');
+    });
+
+   
+    it('should display a message informing that there is no flashcards', function() {
+        expect(element('.empty-list-message').count()).toBe(1);
+    }); 
+
+    it('should not display the contents or navigation', function() {
+        expect(element('.flashcard-buttons').count()).toBe(0);
+    });
+});
